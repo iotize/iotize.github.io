3199a4370f80850d91c6c7698d8badaaacc649a1
Ryan Mason-Davies <iotize@icloud.com>
2020-05-07 21:57:49 +0100

    Rename WebsitePublisher to Publisher

--- Package.swift
+++ Package.swift
@@ -3,16 +3,16 @@
 import PackageDescription
 
 let package = Package(
-    name: "website publisher",
+    name: "publisher",
     products: [
-        .executable(name: "WebsitePublisher", targets: ["WebsitePublisher"]),
+        .executable(name: "Publisher", targets: ["Publisher"]),
     ],
     dependencies: [
         .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", from: "0.1.0"),
     ],
     targets: [
         .target(
-            name: "WebsitePublisher",
+            name: "Publisher",
             dependencies: ["Publish"]),
     ]
 )
