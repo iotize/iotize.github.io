0771a2bfea2282fcf2638ec29f1282c5c8c3df5e
Ryan Mason-Davies <iotize@icloud.com>
2020-05-09 19:00:35 +0100

    Add skeleton website details

--- Sources/Publisher/main.swift
+++ Sources/Publisher/main.swift
@@ -1,3 +1,19 @@
 import Publish
 
-print("Hello World")
+struct PersonalWebsite: Website {
+    enum SectionID: String, WebsiteSectionID {
+        case articles
+        case about
+    }
+
+    struct ItemMetadata: WebsiteItemMetadata {
+    }
+
+    var url = URL(string: "http://www.ryanjdavies.com")!
+    var name = "Ryan's Website"
+    var description = "A website by Ryan."
+    var language: Language { .english }
+    var imagePath: Path? { "images/logo.png" }
+}
+
+try! PersonalWebsite().publish(withTheme: .foundation)
